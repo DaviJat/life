86975e88ebb28ebf4b1a32ec6e205d2b5399a24c
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-01-10 15:31:37 -0300

    Validação inicial com zod

--- src/components/form/SignInForm.tsx
+++ src/components/form/SignInForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { zodResolver } from '@hookform/resolvers/zod';
+import Link from 'next/link';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
@@ -16,8 +17,16 @@ import {
 import { Input } from '../ui/input';
 
 const FormSchema = z.object({
-  email: z.string(),
-  password: z.string(),
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .email('Invalid email'),
+  password: z
+    .string({
+      required_error: 'Password is required',
+    })
+    .min(8, 'Password must have than 8 characters'),
 });
 
 const SignInForm = () => {
@@ -25,41 +34,54 @@ const SignInForm = () => {
     resolver: zodResolver(FormSchema),
   });
 
-  const onSubmit = () => {
-    console.log('Form submit');
+  const onSubmit = (values: z.infer<typeof FormSchema>) => {
+    console.log(values);
   };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter your password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button className="w-full mt-6" type="submit">
+          Sign in
+        </Button>
       </form>
+      <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+        or
+      </div>
+      <p className="text-center text-sm text-gray-600 mt-2">
+        If you don&apos;t have an account, please&nbsp;
+        <Link className="text-blue-500 hover:underline" href="/sign-up">
+          Sign up
+        </Link>
+      </p>
     </Form>
   );
 };
