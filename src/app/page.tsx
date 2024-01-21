881891780257704e4d1f6cb762fc99160ddf980a
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-01-21 20:30:44 -0300

    Estudando funcionamento da client e server session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import User from '@/components/User';
-import { buttonVariants } from '@/components/ui/button';
-import authOptions from '@/lib/auth';
-import { getServerSession } from 'next-auth';
-import Link from 'next/link';
+import User from '@/components/User'; // Componente
+import { buttonVariants } from '@/components/ui/button'; // Constante
+import authOptions from '@/lib/auth'; // Objeto
+import { getServerSession } from 'next-auth'; // Função
+import Link from 'next/link'; // Componente
 
 const Home = async () => {
+  // Obtém a sessão do servidor utilizando a função getServerSession e as opções de autenticação authOptions
   const session = await getServerSession(authOptions);
 
   return (
@@ -13,10 +14,11 @@ const Home = async () => {
       <Link className={buttonVariants()} href="/admin">
         Open My Admin
       </Link>
-
       <h2>Client Session</h2>
+      {/* Renderiza o componente User, exibindo informações sobre o usuário da sessão do cliente */}
       <User />
       <h2>Server Session</h2>
+      {/* Converte a sessão do servidor para uma string JSON e a exibe na página */}
       {JSON.stringify(session)}
     </div>
   );
--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,13 +1,19 @@
-'use client';
+'use client'; // Diretiva do Next.js que indica que este arquivo deve ser executado apenas no lado do cliente.
 
-import { SessionProvider } from 'next-auth/react';
-import { FC, ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react'; // Importa SessionProvider de 'next-auth/react' para gerenciar a sessão do usuário.
+import { FC, ReactNode } from 'react'; // Importa FC (Functional Component) e ReactNode de 'react'.
+
+// Define uma interface para os props do componente Provider.
 interface ProviderProps {
-  children: ReactNode;
+  children: ReactNode; // children é do tipo ReactNode, representando qualquer conteúdo renderizável no React.
 }
 
+// Define o componente funcional Provider.
+// ({ children }) é usado para extrair a propriedade children das props recebidas pelo componente (ProviderProps).
 const Provider: FC<ProviderProps> = ({ children }) => {
+  // Retorna o SessionProvider do NextAuth.js, envolvendo o conteúdo passado como children.
   return <SessionProvider>{children}</SessionProvider>;
 };
 
+// Exporta o componente Provider como padrão.
 export default Provider;
