2f3dc2bc4250bc1738c46fd40f131f2461cffe8b
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-01-21 22:20:36 -0300

    Revert "Estudando funcionamento da client e server session"
    This reverts commit 881891780257704e4d1f6cb762fc99160ddf980a.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,31 @@
-import Navbar from '@/components/Navbar'; // Componente
-import Provider from '@/components/Provider'; // Componente
-import { Toaster } from '@/components/ui/toaster'; // Componente
-import '@/styles/globals.css'; // Arquivo de estilos
-import type { Metadata } from 'next'; // Tipo
-import { Inter } from 'next/font/google'; // Conjunto de fontes 'Inter'do Google Fonts
+import '@/styles/globals.css';
 
-// Configura a fonte 'Inter' para incluir apenas o conjunto de caracteres latinos
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import Navbar from '@/components/Navbar';
+import Provider from '@/components/Provider';
+import { Toaster } from '@/components/ui/toaster';
+
 const inter = Inter({ subsets: ['latin'] });
 
-// Define um objeto de metadados para a página
 export const metadata: Metadata = {
   title: 'Life',
   description: 'System to organize everything I need',
 };
 
-// RootLayout é um componente funcional que recebe um parâmetro 'children' do tipo 'React.ReactNode'.
-// O tipo 'React.ReactNode' é um tipo genérico que pode representar qualquer coisa que pode ser renderizada
-// no React, incluindo elementos JSX, strings e números.
-// O destructuring é utilizado para extrair a propriedade 'children' do objeto de parâmetros passado
-// para a função e atribuí-la à variável local 'children'.
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
     <body className={inter.className}>
-      {/* Componente Provider que envolve toda a aplicação para gerenciar o estado global */}
       <Provider>
-        {/* Elemento principal da aplicação com classes de estilo */}
         <main className="h-screen flex flex-col justify-center items-center">
-          {/* Componente Navbar */}
           <Navbar />
-          {/* Renderiza o conteúdo da página passado como children */}
           {children}
         </main>
-        {/* Componente Toaster que exibe mensagens de notificação na aplicação */}
         <Toaster />
       </Provider>
     </body>
   </html>
 );
 
-// Exporta o componente RootLayout como componente padrão
 export default RootLayout;
--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,19 +1,13 @@
-'use client'; // Diretiva do Next.js que indica que este arquivo deve ser executado apenas no lado do cliente.
+'use client';
 
-import { SessionProvider } from 'next-auth/react'; // Importa SessionProvider de 'next-auth/react' para gerenciar a sessão do usuário.
-import { FC, ReactNode } from 'react'; // Importa FC (Functional Component) e ReactNode de 'react'.
-
-// Define uma interface para os props do componente Provider.
+import { SessionProvider } from 'next-auth/react';
+import { FC, ReactNode } from 'react';
 interface ProviderProps {
-  children: ReactNode; // children é do tipo ReactNode, representando qualquer conteúdo renderizável no React.
+  children: ReactNode;
 }
 
-// Define o componente funcional Provider.
-// ({ children }) é usado para extrair a propriedade children das props recebidas pelo componente (ProviderProps).
 const Provider: FC<ProviderProps> = ({ children }) => {
-  // Retorna o SessionProvider do NextAuth.js, envolvendo o conteúdo passado como children.
   return <SessionProvider>{children}</SessionProvider>;
 };
 
-// Exporta o componente Provider como padrão.
 export default Provider;
