6019c1f693f7d694fc47ebf72607f95c80e9d7f5
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-01-22 21:41:55 -0300

    Organização das pastas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import User from '@/components/User';
-import { buttonVariants } from '@/components/ui/button';
 import authOptions from '@/lib/auth';
 import { getServerSession } from 'next-auth';
-import Link from 'next/link';
 
 const Home = async () => {
   const session = await getServerSession(authOptions);
@@ -10,14 +7,6 @@ const Home = async () => {
   return (
     <div>
       <h1 className="text-4xl">Home</h1>
-      <Link className={buttonVariants()} href="/admin">
-        Open My Admin
-      </Link>
-
-      <h2>Client Session</h2>
-      <User />
-      <h2>Server Session</h2>
-      {JSON.stringify(session)}
     </div>
   );
 };
