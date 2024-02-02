642616a90f1beeaa7c9a8280bfb8959bdee65d67
DaviJat <91758001+DaviJat@users.noreply.github.com>
2024-02-02 16:57:19 -0300

    Criando rotas para carteira

---
+++ src/app/(dashboard)/financeiro/carteira/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+  return <div>Carteira</div>;
+}
+
+export default page;
--- src/app/(dashboard)/financeiro/page.tsx
+++ src/app/(dashboard)/financeiro/page.tsx
@@ -1,12 +1,21 @@
 import authOptions from '@/lib/auth';
 import { getServerSession } from 'next-auth';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 const page = async () => {
   const session = await getServerSession(authOptions);
 
   if (session?.user) {
-    return <div>Financeiro</div>;
+    return (
+      <div>
+        <Link href={'financeiro/carteira'}>Carteira</Link>
+        <Link href={'financeiro/entradaCarteira'}>Entrada</Link>
+        <Link href={'financeiro/saidaCarteira'}>Saída</Link>
+        <Link href={'financeiro/contasAPagar'}>Contas a pagar</Link>
+        <Link href={'financeiro/contasAReceber'}>Contas a receber</Link>
+      </div>
+    );
   }
   return redirect('/sign-in');
 };
