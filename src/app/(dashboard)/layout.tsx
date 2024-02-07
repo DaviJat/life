bbc7df7476d6d6f01eb4577179501643d2e158aa
DaviJat <91758001+DaviJat@users.noreply.github.com>
2024-02-07 13:42:52 -0300

    Estrutura para layout mobile e desktop

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,8 +1,9 @@
-import Navbar from '@/components/layout/Navbar';
-import Sidebar from '@/components/layout/Sidebar';
-import { FC, ReactNode } from 'react';
+'use client';
 
-import SidebarItem from '@/components/layout/SidebarItem';
+import Navbar from '@/components/layout/Navbar';
+import { FC, ReactNode, useState } from 'react';
+
+import NavbarItem from '@/components/layout/NavbarItem';
 import { CircleDollarSign, ClipboardList, Home } from 'lucide-react';
 
 interface DashboardLayoutProps {
@@ -10,18 +11,37 @@ interface DashboardLayoutProps {
 }
 
 const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  window.addEventListener('resize', () => {
+    setIsMobile(window.innerWidth < 768);
+  });
+
   return (
-    <div className="inline-flex w-screen">
-      <Sidebar>
-        <SidebarItem icon={<Home size={20} />} text="Home" route="/home" />
-        <SidebarItem icon={<ClipboardList size={20} />} text="Tarefas" route="/task" />
-        <SidebarItem icon={<CircleDollarSign size={20} />} text="Financeiro" route="/finance" />
-      </Sidebar>
-      <div className="w-full">
-        <Navbar />
-        <div className="container">{children}</div>
-      </div>
-    </div>
+    <>
+      {isMobile ? (
+        <div>
+          <div>conteudo</div>
+          <div className="fixed bottom-0 w-full">
+            <Navbar>
+              <NavbarItem icon={<CircleDollarSign size={20} />} text="Financeiro" route="/finance" />
+              <NavbarItem icon={<Home size={20} />} text="Home" route="/home" />
+              <NavbarItem icon={<ClipboardList size={20} />} text="Tarefas" route="/task" />
+            </Navbar>
+          </div>
+        </div>
+      ) : (
+        <div className="inline-flex w-screen">
+          <Navbar>
+            <NavbarItem icon={<Home size={20} />} text="Home" route="/home" />
+            <NavbarItem icon={<ClipboardList size={20} />} text="Tarefas" route="/task" />
+            <NavbarItem icon={<CircleDollarSign size={20} />} text="Financeiro" route="/finance" />
+          </Navbar>
+          <div className="w-full">
+            <div className="container">{children}</div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,13 +1,67 @@
-import authOptions from '@/lib/auth';
-import { getServerSession } from 'next-auth';
+'use client';
 
-const Navbar = async () => {
-  const session = await getServerSession(authOptions);
+import { ChevronFirst, ChevronLast, MoreVertical } from 'lucide-react';
+import { ReactNode, createContext, useState } from 'react';
+
+interface NavbarProps {
+  children: ReactNode;
+}
+
+export const NavbarContext = createContext({ expanded: true });
+
+const Navbar = ({ children }: NavbarProps) => {
+  const [expanded, setExpanded] = useState(true);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  window.addEventListener('resize', () => {
+    setIsMobile(window.innerWidth < 768);
+  });
 
   return (
-    <div className="flex border-b p-4">
-      <p>Navbar</p>
-    </div>
+    <>
+      {isMobile ? (
+        <div className="flex justify-around bg-gray-900">{children}</div>
+      ) : (
+        <aside className="h-screen">
+          <nav className="h-full inline-flex flex-col bg-white border-r shadow-sm">
+            <div className="p-4 pb-2 flex justify-between items-center">
+              <img
+                src="https://img.logoipsum.com/243.svg"
+                className={`overflow-hidden transition-all ${expanded ? 'w-32' : 'w-0'}`}
+                alt=""
+              />
+              <button
+                onClick={() => setExpanded((curr) => !curr)}
+                className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+              >
+                {expanded ? <ChevronFirst /> : <ChevronLast />}
+              </button>
+            </div>
+            <NavbarContext.Provider value={{ expanded }}>
+              <ul className="flex-1 px-3">{children}</ul>
+            </NavbarContext.Provider>
+            <div className="border-t flex p-3">
+              <img
+                src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+                alt=""
+                className="w-10 h-10 rounded-md"
+              />
+              <div
+                className={`
+              flex justify-between items-center
+              overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0'}
+          `}
+              >
+                <div className="leading-4">
+                  <h4 className="font-semibold">John Doe</h4>
+                  <span className="text-xs text-gray-600">johndoe@gmail.com</span>
+                </div>
+                <MoreVertical size={20} />
+              </div>
+            </div>
+          </nav>
+        </aside>
+      )}
+    </>
   );
 };
 
