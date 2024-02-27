bdc280fc3b07ff19ef7e58d73ea2c63aba61e0e5
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-02-27 10:36:31 -0300

    Criando páginas e arquivos

---
+++ src/components/tables/wallet/columns.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { MoneyLocation } from '@prisma/client';
+import { ColumnDef } from '@tanstack/react-table';
+import { ChevronsUpDown, MoreHorizontal } from 'lucide-react';
+
+export const columns: ColumnDef<MoneyLocation>[] = [
+  {
+    accessorKey: 'id',
+    header: ({ column }) => {
+      return (
+        <Button className="px-2" variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+          Id
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className="px-2">{row.getValue('id')}</div>;
+    },
+  },
+  {
+    accessorKey: 'description',
+    header: ({ column }) => {
+      return (
+        <Button className="px-2" variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}>
+          Descrição
+          <ChevronsUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <div className="px-2">{row.getValue('description')}</div>;
+    },
+  },
+  {
+    accessorKey: 'type',
+    header: 'Tipo',
+    cell: ({ row }) => {
+      const type = row.getValue('type');
+      const typeTranslated = type === 'Physical' ? 'Físico' : type === 'Virtual' ? 'Virtual' : type;
+      return typeTranslated;
+    },
+  },
+  {
+    accessorKey: 'createdAt',
+    header: 'Data cadastro',
+    cell: ({ row }) => {
+      const date = new Date(row.getValue('createdAt'));
+      const formatted = date.toLocaleDateString();
+      return formatted;
+    },
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => (window.location.href = '/finance/money-location/' + payment.id)}>
+              Editar
+            </DropdownMenuItem>
+            {/* <DropdownMenuItem onClick={() => console.log('Delete', payment.id)}>Excluir</DropdownMenuItem> */}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
