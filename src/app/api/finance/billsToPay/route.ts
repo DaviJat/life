24ce4d26343b63a4ebf442ad4791ceda7191430f
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-08-05 16:55:08 -0300

    Função POST

--- src/app/api/finance/billsToPay/route.ts
+++ src/app/api/finance/billsToPay/route.ts
@@ -1,9 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
 
 import db from '@/lib/db';
 
+// Define um schema utilizando a biblioteca Zod para validar os dados recebidos nas requisições.
+const userSchema = z.object({
+  description: z.string().min(1).max(30),
+  value: z.number().max(9999999999999),
+  personId: z.number()
+});
+
 // Função assíncrona para lidar com requisições GET.
-export default async function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   // Obtém o parâmetro 'id' da URL da requisição.
   const id = request.nextUrl.searchParams.get('id');
   try {
@@ -35,5 +43,37 @@ export default async function GET(request: NextRequest) {
       },
       { status: 500 }
     );
+  }
+}
+
+// Função assíncrona para lidar com requisições POST.
+export async function POST(request: NextRequest) {
+  try {
+    // Obtém o corpo da requisição POST.
+    const body = await request.json();
+
+    // Valida o corpo da requisição com o schema definido anteriormente.
+    const { description, value, personId } = userSchema.parse(body);
+
+    // Cria um novo registro de billsToPay no banco de dados com os dados recebidos.
+    const newBillsToPay = await db.billsToPay.create({
+      data: {
+        description,
+        value,
+        personId
+      }
+    });
+
+    // Retorna uma resposta de sucesso com o novo registro criado.
+    return NextResponse.json(
+      {
+        wallet: newBillsToPay,
+        message: 'Conta a pagar cadastrada com sucesso'
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    // Retorna uma resposta de erro caso ocorra uma exceção durante o processamento da requisição.
+    return NextResponse.json({ message: error }, { status: 500 });
   }
 }
