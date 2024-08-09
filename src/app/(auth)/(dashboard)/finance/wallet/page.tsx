aef9bff9f971f27f9ccf0d3f500c26b56fe8bc11
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-08-09 10:07:14 -0300

    Ajustando no front

--- src/app/(auth)/(dashboard)/finance/wallet/page.tsx
+++ src/app/(auth)/(dashboard)/finance/wallet/page.tsx
@@ -1,12 +1,8 @@
 import Table from '@/components/tables/wallet/Table';
-import authOptions from '@/lib/auth';
-import { getServerSession } from 'next-auth';
 
 // Recupera função para recuperar dados da api
 async function getData() {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-  const response = await fetch(process.env.URL + `/api/finance/wallet?userId=${user.id}`, { cache: 'no-store' });
+  const response = await fetch(process.env.URL + '/api/finance/wallet', { cache: 'no-store' });
   return await response.json();
 }
 
