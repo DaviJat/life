9dbd81b2b545826e2c58128e20eceaaa3184f7dc
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-08-20 19:41:09 -0300

    Estilizando

--- src/components/layout/desktop/NavbarCollapsible.tsx
+++ src/components/layout/desktop/NavbarCollapsible.tsx
@@ -1,18 +1,64 @@
-import { ReactNode } from 'react';
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { ReactNode, useContext, useState } from 'react';
+import { NavbarContext } from './Navbar';
 
 interface NavbarCollapsibleProps {
   icon: ReactNode;
   text: string;
-  options: Array<string>;
+  options?: Array<string>;
 }
 
 function NavbarCollapsible({ icon, text, options }: NavbarCollapsibleProps) {
+  const { expanded } = useContext(NavbarContext);
+
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
+
+  const toggleSubMenu = () => {
+    setSubMenuOpen(!subMenuOpen);
+  };
+
+  const pathname = usePathname();
+  const isActive = true;
   return (
-    <div>
-      <li>{options[0]}</li>
-      <li>{options[1]}</li>
-      <li>{options[2]}</li>
-      <li>{options[3]}</li>
+    <div onClick={toggleSubMenu}>
+      <li
+        className={`
+          relative flex items-center py-2 px-3 my-1
+          font-medium rounded-md cursor-pointer
+          transition-colors group
+          hover:bg-surface-hover text-surface-foreground
+        `}
+      >
+        {icon}
+        <span className={`overflow-hidden transition-all ${expanded ? 'w-28 ml-3' : 'w-0'}`}>{text}</span>
+
+        {!expanded && (
+          <div
+            className={`
+              absolute left-full rounded-md px-2 py-1 ml-6
+              bg-surface text-surface-foreground text-sm
+              invisible opacity-80 -translate-x-3 transition-all
+              group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
+          `}
+          >
+            {text}
+          </div>
+        )}
+      </li>
+      {subMenuOpen && (
+        <div
+          className={`
+          relative flex items-center py-2 px-3 my-1
+          font-medium rounded-md cursor-pointer
+          transition-colors group
+          hover:bg-surface-hover text-surface-foreground
+        `}
+        >
+          teste
+        </div>
+      )}
     </div>
   );
 }
