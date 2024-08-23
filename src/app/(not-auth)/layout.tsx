1ede374c737d9b94aa3f1fd361ee307c8c16c68d
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2024-08-22 22:12:16 -0300

    Ajuste parte branca no login mobile

--- src/app/(not-auth)/layout.tsx
+++ src/app/(not-auth)/layout.tsx
@@ -1,17 +1,23 @@
+'use client';
+import { DeviceContext } from '@/components/providers/DeviceProvider'; // Ajuste o caminho conforme necessário
 import Image from 'next/image';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useContext } from 'react';
 
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
-const AuthLayout: FC<AuthLayoutProps> = ({ children }) => (
-  <div className="bg-surface min-h-screen">
-    <div className="container flex flex-col items-center pt-12 pb-4 w-auto">
-      <Image src="/images/logo-fundo-transparente.png" width={200} height={100} alt="Life" priority />
-      <div className="w-full">{children}</div>
+const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
+  const { isMobile } = useContext(DeviceContext);
+
+  return (
+    <div className="bg-surface min-h-screen">
+      <div className={`container flex flex-col items-center pt-12 pb-4 ${isMobile ? 'w-auto' : 'w-96'}`}>
+        <Image src="/images/logo-fundo-transparente.png" width={200} height={100} alt="Life" priority />
+        <div className="w-full">{children}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AuthLayout;
